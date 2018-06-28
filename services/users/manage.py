5de8b988dd32198d0c3df5a9c1a34e8faeba9ce8
Adil Sadik <sparky.005@gmail.com>
2018-06-27 21:54:00 -0400

    Add testing

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,6 +2,7 @@
 
 from flask.cli import FlaskGroup
 
+import unittest
 from project import app, db
 
 cli = FlaskGroup(app)
@@ -11,6 +12,15 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+@cli.command()
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
